3f7e45b8d2fb01db9caa843c5a9d374ffe91b385
paulomelila <paulovitor22@hotmail.com>
2017-09-04 13:48:58 -0700

    update comments

--- Assignment 15/Assignment 15/main.m
+++ Assignment 15/Assignment 15/main.m
@@ -9,24 +9,21 @@
 /*
  SNAKES & LADDERS
  
- To create a single player command line game called Snakes & Ladders
- that models the kid's game
+ To create a multiplayer version of the game called Snakes & Ladders
  
+ ● The app will wait for the user to type the number of players
  ● The app will wait for the user to type roll (or r for short) and will roll a
  random dice value between 1 and 6
+ ● The game will display the current score after every roll
  ● Once the user rolls the app will move the user to a new square
  ● The user will jump forward or go back depending whether they land on a
  ladder or snake
  ● The app will inform the user which square they have landed on and
  whether they hit a snake or ladder along the way
- ● When the user passes square 100 the game is over and the game and
+ ● When any user passes square 100 the game is over and the game and
  congratulates them.
  
- 1. Create a NSDictionary and populate it with NSNumber variables.
- 2. Compare NSNumber variables
- 3. Use dictionary for modeling moving squares when landing on ladders or snakes
- 4. Override the default initializer init
- */
+*/
 
 #import <Foundation/Foundation.h>
 #import "InputHandler.h"
